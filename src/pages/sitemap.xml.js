765f0bd280d087b45b4aa329124bd92736a723ba
ayaz qureshi <qureshil.ayaz29@gmail.com>
2025-11-06 15:57:09 +0530

    fix seo for the custom domain

--- src/pages/sitemap.xml.js
+++ src/pages/sitemap.xml.js
@@ -1,5 +1,7 @@
+import { SITE_URL } from '../site.config.mjs';
+
 export async function GET() {
-  const body = `<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n  <url>\n    <loc>https://ayazqureshi.vercel.app/</loc>\n    <changefreq>monthly</changefreq>\n    <priority>1.0</priority>\n  </url>\n</urlset>`;
+  const body = `<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n  <url>\n    <loc>${SITE_URL}/</loc>\n    <changefreq>monthly</changefreq>\n    <priority>1.0</priority>\n  </url>\n</urlset>`;
 
   return new Response(body, {
     headers: {
