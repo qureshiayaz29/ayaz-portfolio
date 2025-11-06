9b98ee79d814d68c5fdf9961e8ca3fe2d6f04218
ayaz qureshi <qureshil.ayaz29@gmail.com>
2025-11-06 16:25:21 +0530

    add telegram bot link

--- src/site.config.mjs
+++ src/site.config.mjs
@@ -1,1 +1,4 @@
-export const SITE_URL = process.env.SITE_URL || 'https://ayazqureshi.com';
+const VERCEL_URL = process.env.VERCEL_URL;
+export const SITE_URL =
+  process.env.SITE_URL || (VERCEL_URL ? `https://${VERCEL_URL}` : 'https://ayazqureshi.com');
+
